fa0e7a96b7bdae7bbefbe73c5323836e3045989b
Tony Brix <Tony@Brix.ninja>
2016-09-01 10:37:45 -0500

    add failing no tests

--- spec/semicolonize-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SemicolonizeView from '../lib/semicolonize-view';
-
-describe('SemicolonizeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
