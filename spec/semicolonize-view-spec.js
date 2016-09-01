ade9bdd4cf69125f2fea3730e2c709fb8c01cb2a
Tony Brix <Tony@Brix.ninja>
2016-09-01 10:32:49 -0500

    init

---
+++ spec/semicolonize-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SemicolonizeView from '../lib/semicolonize-view';
+
+describe('SemicolonizeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
